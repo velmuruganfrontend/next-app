1255338952b7d2212ed292e8afd1c64608ad4c87
velmuruganfrontend <velmuruganfrontenddeveloper@gmail.com>
2020-10-03 11:01:09 +0530

    Updated index file
    Updated index file

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
           rel="noopener noreferrer"
           class="button--green"
         >
-          Documentation
+          Documentation Vel
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
